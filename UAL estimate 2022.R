2a43d42dff7900a3268bf5bbf2819eea85f7db2b
Truong Bui <truongbui@Truongs-MacBook-Pro.local>
2022-06-18 03:25:11 -0700

    Add ual per capita and fix some data

--- UAL estimate 2022.R
+++ UAL estimate 2022.R
@@ -16,6 +16,8 @@ ppd_full <- read.csv("ppd-data-latest-05-2022.csv")
 #Import index returns data
 index_returns <- read.csv("./Index returns data/index_returns.csv")
 
+#Import population data
+uspop <- import("State population.xlsx")
 
 #Specify fiscal years
 pre_fy <- 2020
@@ -50,6 +52,21 @@ ppd <- ppd %>%
   mutate(nc = replace(nc, plan_name == "New York State Teachers" & fy == 2020, 0.1),
          nc = replace(nc, plan_name == "NY State & Local ERS" & fy == 2020, 0.17),
          nc = replace(nc, plan_name == "NY State & Local Police & Fire" & fy == 2020, 0.3))
+
+#Fix wrong data:
+ppd <- ppd %>% 
+  mutate(aal = replace(aal, plan_name == "Nebraska Schools" & fy == 2001, 4576863))
+
+#Missouri Local Government Employees Retirement System
+#Arkansas Public Employees Retirement System
+#Nebraska Public Employees Retirement System - School Employees Plan
+#North Carolina Local Governmental Employees' Retirement System
+#Employees' Retirement System of the State of Hawaii
+#Massachusetts State Retirement System
+#State Employees' Retirement System of Illinois
+#Massachusetts Teachers' Retirement System
+#Teachers' and State Employees' Retirement System of North Carolina
+
 
 
 #Identify plans that have the same AAL for current fy and previous fy
@@ -181,24 +198,24 @@ ppd_project <- ppd %>%
 #https://henrywang.nl/another-quadratic-programming-example-with-r/
 
 benchmark_portfolio <- function(return, x1, x2, x3) {
-
-end_pos <- max(which(!is.na(return)))       #find the latest year with available return data
-y_actual <- return[(end_pos - 9):end_pos]   #use only the last 10 years (with available return data) to create the benchmark
-x0 <- 1                                     #for the intercept (alpha)
-x1 <- x1[(end_pos - 9):end_pos]
-x2 <- x2[(end_pos - 9):end_pos]
-x3 <- x3[(end_pos - 9):end_pos]
-x = cbind(x0, x1, x2, x3)
-
-Dmat <- crossprod(x)
-dvec <- crossprod(y_actual, x)                  # vector to be minimized: product:transpose y_actual and x
-# Amat <- cbind(rep(1,3), diag(3))   
-Amat <- t(cbind(0, rbind(rep(1,3), diag(3))))   # matrix defining the constraints
-bvec <- c(1,0,0,0)                              # vector of b coefficient; meq = 1 is equality constraint: coefs sum to 1  
-
-result <- solve.QP(Dmat = Dmat, dvec = dvec, Amat = Amat, bvec = bvec, meq = 1) 
-
-return(list(result$solution))
+  
+  end_pos <- max(which(!is.na(return)))       #find the latest year with available return data
+  y_actual <- return[(end_pos - 9):end_pos]   #use only the last 10 years (with available return data) to create the benchmark
+  x0 <- 1                                     #for the intercept (alpha)
+  x1 <- x1[(end_pos - 9):end_pos]
+  x2 <- x2[(end_pos - 9):end_pos]
+  x3 <- x3[(end_pos - 9):end_pos]
+  x = cbind(x0, x1, x2, x3)
+  
+  Dmat <- crossprod(x)
+  dvec <- crossprod(y_actual, x)                  # vector to be minimized: product:transpose y_actual and x
+  # Amat <- cbind(rep(1,3), diag(3))   
+  Amat <- t(cbind(0, rbind(rep(1,3), diag(3))))   # matrix defining the constraints
+  bvec <- c(1,0,0,0)                              # vector of b coefficient; meq = 1 is equality constraint: coefs sum to 1  
+  
+  result <- solve.QP(Dmat = Dmat, dvec = dvec, Amat = Amat, bvec = bvec, meq = 1) 
+  
+  return(list(result$solution))
 }
 
 
@@ -283,4 +300,52 @@ ppd_project_us <- ppd_project_final %>%        #national
             us_ual = us_aal - us_mva,
             us_funded_ratio = us_mva/us_aal)
 
+#Calculate unfunded liability per capita by state
+uspop_long <- uspop %>% 
+  mutate(`2022` = `2021`) %>% 
+  pivot_longer(cols = 2:24, names_to = "fy", values_to = "pop") %>% 
+  mutate(fy = as.numeric(fy)) %>% 
+  rename(state = State) %>% 
+  arrange(state, fy)
+
+ppd_project_state_capita <- ppd_project_state %>% 
+  left_join(uspop_long) %>% 
+  mutate(state_ual_capita = state_ual/pop * 1000)
+
+# library(echarts4r)
+
+# ppd_project_us %>% 
+#   mutate(us_ual_real = ifelse(fy <= 2020, us_ual, NA),
+#          fy = as.character(fy)) %>% 
+#   e_charts(fy) %>% 
+#   e_line(us_ual, symbolSize = 7, lineStyle = list(width = 3, type = "dotted")) |>     #plot the dotted line first and the solid line later so that the solid line will block the dots
+#   e_line(us_ual_real, symbolSize = 7, lineStyle = list(width = 3)) |>
+#   # e_theme_custom("echarts_theme.json") |>
+#   e_title("Unfunded Liability") |>
+#   e_tooltip(trigger = "item",
+#             formatter = e_tooltip_item_formatter("currency", digits = 0)) |>
+#   e_legend(show = F) |>
+#   e_color("orange")
+
+
+
+# 
+# chosen_plan <- c("Pennsylvania Municipal", "Alabama ERS")
+# ppd_project_final %>% 
+#   filter(plan_name %in% chosen_plan) %>% 
+#   group_by(plan_name) %>% 
+#   mutate(ual_real = ifelse(real_vs_estimate == "real", ual, NA)) |>
+#   # mutate(ual_estimate = ifelse(real_vs_estimate == "estimate", ual, NA)) |>
+#   mutate(fy = as.character(fy)) |>
+#   e_charts(fy) |>
+#   e_line(ual, symbolSize = 7, lineStyle = list(width = 3, color = "gray", type = "dotted")) |>
+#   e_line(ual_real, symbolSize = 7, lineStyle = list(width = 3)) |>
+#   # e_theme_custom("echarts_theme.json") |>
+#   e_title("UAL") |>
+#   e_tooltip(trigger = "item",
+#             formatter = e_tooltip_item_formatter("currency", digits = 0))
+# 
+
+
+  
 
